f1f1386a9fbbca9570b479005cc06fe605257dfc
Ambar1418 <itsambar14@gmail.com>
2026-05-02 14:53:34 +0530

    update vite link latest

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-08fbdd75'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.8i0vcprdl1"
+    "revision": "0.bftdpt4udu8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
